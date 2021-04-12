a40c83dfcf3ceff849d0d68bf91fb33456d63e5e
pluu2 <lulugao@Lulus-MacBook-Air.local>
2021-04-12 15:19:46 -0400

    updated client.py testclient.py

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 class client (): 
     def __init__(self,ip,port): 
         self.ip=ip
@@ -10,5 +11,15 @@ class client ():
     def connect(self): 
         self.s.connect((self.ip,self.port))
         print('connected')
-        print(self.s.recv(1024))
+        
+        
+        data = b''
+        print('receiving')
+        
+        print ('.')
+        block=self.s.recv(1000000) #this has to be adjusted for image
+        data= block    
         self.s.close() 
+        #print(data)
+        #image=pickle.loads(data)
+        return data
--- testclient.py
+++ testclient.py
@@ -1,10 +1,27 @@
-import sysconfig 
+import sys
+import pickle
+#print(sys.version)
 
+import sysconfig 
+import numpy as np
 from sysconfig import ip,port
 import src.client 
 from src.client import client
+import matplotlib as py
+from matplotlib import pyplot as py
 
 newclient =client(ip,port) 
 newclient.createsocket()
-newclient.connect()
+temp=newclient.connect() #return a massive pickle file
+print('received something')
+print(temp)
+
+
+#py.imshow(temp.reshape(320,304,3))
+#i = np.arange(320*304).reshape(320, 304)
+
+#nim=np.frombuffer(image, dtype=i.dtype)
+
+#print(nim.shape)
+
 
